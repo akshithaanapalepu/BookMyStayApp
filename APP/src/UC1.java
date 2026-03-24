1640e5ad723ce377a277faf38c04fe9311c3194f
akshithaanapalepu <akshitharam06@gmail.com>
2026-03-24 15:30:33 +0530

    Welcome Message

---
+++ APP/src/UC1.java
@@ -0,0 +1,9 @@
+public class UC1 {
+
+    public static void main(String[] args) {
+// Display welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
+
